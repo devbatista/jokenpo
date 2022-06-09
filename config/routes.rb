0e9cac7073e55791f38ade4b035465795f10669b
Rafael Batista <rafael@devbatista.com>
2022-06-09 18:33:38 -0300

    Criando rota de autenticação e resource de usuários

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
+    resources: :users, param: :_username
+    
+    post '/login', to: 'authentication#login'
+
     post '/jogar', to: 'v1/plays#index'
   end
 end
