3198b1e7c8c1686aa4e80be003f5731ab666e91f
Rafael Batista <rafael@devbatista.com>
2022-06-10 08:46:11 -0300

    Criando a requisição para autorização do jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+  def authorize_request
+    authorization = request.headers['Authorization']
+    authorization = authorization.split(' ').last if authorization
+    begin
+      @decoded = JsonWebToken.decode(authorization)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { error: e.message }, status: :unauthorized
+    end
+  end
 end
