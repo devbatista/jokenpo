5cd9152c61664ecc4d0c02d1e8613b90d91e0941
Rafael Batista <rafael@devbatista.com>
2022-06-10 14:40:32 -0300

    Renovando coluna admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_115523) do
+ActiveRecord::Schema.define(version: 2022_06_10_173000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_06_10_115523) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
