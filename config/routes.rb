c887cb842fa9ab6d4e1f42867e5090f55ecbf650
Rafael Batista <rafael@devbatista.com>
2022-06-10 08:39:43 -0300

    Corrigindo rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources: :users, param: :_username
-    
+    resources :users, param: :_username
+
     post '/login', to: 'authentication#login'
 
     post '/jogar', to: 'v1/plays#index'
